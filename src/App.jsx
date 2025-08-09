020fcd01de5d12a5c49dbe521cef881d9fa033e9
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-08-09 13:33:19 +0100

    footer content added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./styles/main.scss"
 export default function App() {
   return (
     <div id="app" className="app-wrapper">
-    <div className="sand-texture"></div>
+      <div className="sand-texture"></div>
       <div className="header">
-        
+
       </div>
       <div className="footer">
-
+        <div className="footer-content">
+          <h1 className="footer--heading heading">COMING SOON<span> <span>12</span> <span>03</span> <span>25</span></span></h1>
+          <p className="footer--body-text body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+        </div>
       </div>
     </div>
   )
