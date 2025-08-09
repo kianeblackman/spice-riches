eadd03717232d1de16b4929acdb8f5bebcd322aa
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-08-09 13:18:51 +0100

    texter overlay added and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./styles/main.scss"
 export default function App() {
   return (
     <div id="app" className="app-wrapper">
+    <div className="sand-texture"></div>
+      <div className="header">
+        
+      </div>
+      <div className="footer">
 
+      </div>
     </div>
   )
 }
