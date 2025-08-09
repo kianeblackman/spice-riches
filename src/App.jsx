683dff7133a5c10b9277781767fe4c941f608c95
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-08-09 13:08:24 +0100

    background image set

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import "./styles/main.scss"
 export default function App() {
   return (
     <div id="app" className="app-wrapper">
-      <h1 className="heading">Hello World</h1>
-      <p className="paragraph">Welcome to space</p>
+
     </div>
   )
 }
