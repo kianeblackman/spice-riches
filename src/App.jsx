b5208d544648fa4ceefbc043fe7f19a1639e32da
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-08-09 12:54:58 +0100

    audiowide and quicksand fonts imported and rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import "./styles/main.scss"
 
 export default function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div id="app" className="app-wrapper">
+      <h1 className="heading">Hello World</h1>
+      <p className="paragraph">Welcome to space</p>
+    </div>
   )
 }
