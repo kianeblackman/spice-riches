6d40c41574c98cb27660a80b68ab2b48319404ac
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-08-10 00:56:50 +0100

    updated image sizes

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,24 @@ import chani from "./assets/img/chani.png";
 import baron from "./assets/img/baron.png";
 
 const images = [
-  { src: paul, alt: "Character Paul", className: "character-slider--image" },
-  { src: chani, alt: "Spaceship", className: "character-slider--image" },
-  { src: baron, alt: "Logo Character", className: "character-slider--image" },
+  { src: paul, alt: "Character Paul", className: "character-slider-image" },
+  { src: chani, alt: "Character Chani", className: "character-slider-image" },
+  { src: baron, alt: "Character Baron", className: "character-slider-image" },
 ];
 
 export default function App() {
   const [currentCharacter, setCurrentCharacter] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentCharacter((prevIndex) => (prevIndex + 1) % images.length);
+    const interval = setInterval(function () {
+      setCurrentCharacter(function (prevIndex) {
+        const nextIndex = prevIndex + 1;
+        if (nextIndex === images.length) {
+          return 0;
+        } else {
+          return nextIndex;
+        }
+      });
     }, 3000);
 
     return () => clearInterval(interval);
@@ -31,21 +38,16 @@ export default function App() {
     <div id="app" className="app-wrapper">
       <div className="sand-texture"></div>
       <div className="header">
-        <div className="header--content">
+        <div className="header-content">
+          <h1 className="header-heading heading">SPIN FOR SPICE & <br></br>RULE THE DESERT!</h1>
           <div className="header-spaceship">
-            <img
-              src={spaceship}
-              alt="Spice Riches Spaceship"
-              className="header-spaceship-img"
-            />
+            <img src={spaceship} alt="Spice Riches Spaceship" className="header-spaceship-img" />
           </div>
-          <h1 className="header--heading heading">SPIN FOR SPICE</h1>
-          <h1 className="header--heading heading">RULE THE DESERT</h1>
         </div>
       </div>
       <div className="character-slider">
         <div className="character-slider-content">
-          <div className="character-slider--item">
+          <div className="character-slider-item">
             <img
               src={currentImage.src}
               alt={currentImage.alt}
@@ -56,10 +58,10 @@ export default function App() {
       </div>
       <div className="footer">
         <div className="footer-content">
-          <div className="footer--logo">
-            <img src={logo} alt="Spice Riches Logo" className="footer--logo-image" />
+          <div className="footer-logo">
+            <img src={logo} alt="Spice Riches Logo" className="footer-logo-image" />
           </div>
-          <h1 className="footer--heading heading">
+          <h1 className="footer-heading heading">
             COMING SOON
             <span>
               {" "}
@@ -68,7 +70,7 @@ export default function App() {
               <span className="footer-countdown">25</span>
             </span>
           </h1>
-          <p className="footer--body-text body-text">
+          <p className="footer-body-text body-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
             ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
